f22717a9524aae701920c2d5844d89c05b4f8e29
chukajohn <chukajohn4@gmail.com>
2023-12-15 02:13:26 -0800

    beautiful betty

---
+++ README.md
@@ -0,0 +1,1 @@
+this my alx simple shell project
---
+++ bety.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *add_num - prints the sum of two integer
+ *@a: first integer
+ *@b: second integer
+ *Return: always 0
+ */
+
+int add_num(int a, int b)
+{
+	printf("the sum of this num is: %s\n");
+	return (a + b);
+}
